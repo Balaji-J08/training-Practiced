131589ee6d73579c83d7a8428acd59efd3695152
Balaji-J08 <balajiraoj08@gmail.com>
2018-08-04 09:52:07 +0530

    First assessment

---
+++ FIRST ASSESSMENT/ADO.netJOINTS/ADOnet1/ADOnet1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace ADOnet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().CreateTable();
+            Console.ReadLine();
+        }
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            
+
+            try
+            {
+                //con = new SqlConnection("Data Source=DESKTOP-4EEFVUL\\SQLEXPRESS;Initial Catalog=Employee;Integrated Security=True");
+                // Creating Connection  
+                con = new SqlConnection("data source=DELL\\SQLEXPRESS; database = employe; integrated security=SSPI");//instead we can put integrated security= True
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("create table employe(id int not null, name varchar(100), email varchar(50), join_date Date)", con);  
+                // Opening Connection  
+                con.Open();
+                // Executing the SQL query  
+                cm.ExecuteNonQuery();
+                // Displaying a message  
+                Console.WriteLine("Table created Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
---
+++ FIRST ASSESSMENT/server clients/Client1/Client5.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client1
+{
+    class Client5
+    {
+        static void Main(string[] args)
+        {
+            Socket sok = new Socket(AddressFamily.InterNetwork,SocketType.Stream,ProtocolType.Tcp);
+            IPEndPoint ipe = new IPEndPoint( IPAddress.Parse("127.0.0.1"), 2055);
+            sok.Connect(ipe);
+
+            while (true)
+            {
+
+                byte[] MsgBuffer = new byte[255];
+                int rec = sok.Receive(MsgBuffer, 0, MsgBuffer.Length, 0);
+                Array.Resize(ref MsgBuffer, rec);
+
+                Console.WriteLine("SERVER:{0}", Encoding.Default.GetString(MsgBuffer));
+
+              
+
+                Console.WriteLine("Enter any thing: ");
+                String msg = Console.ReadLine();
+                if(msg=="exit")
+                {
+                    break;
+                }
+                byte[] buffer = Encoding.Default.GetBytes(msg);
+                sok.Send(buffer, 0, buffer.Length, 0);
+
+               
+
+
+            }
+
+            
+            Console.ReadLine();
+            sok.Close();
+
+
+
+
+
+        }
+
+
+    }
+}
