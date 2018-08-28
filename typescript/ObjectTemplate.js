9130a3cf1c7450985b12b99abf939b6255e6c965
Balaji-J08 <balajiraoj08@gmail.com>
2018-08-28 10:21:13 +0530

    code

--- typescript/ObjectTemplate.js
+++
@@ -1,9 +0,0 @@
-var person1 = {
-    firstName: "Tom",
-    lastName: "Hanks",
-    sayHello: function () { } //Type template 
-};
-person1.sayHello = function () {
-    console.log("hello " + person1.firstName);
-};
-person1.sayHello();
--- typescript/multipleInheritance.js
+++
@@ -1,2 +0,0 @@
-var Iobj = { v1: 12, v2: 23 };
-console.log("value 1: " + this.v1 + " value 2: " + this.v2);
